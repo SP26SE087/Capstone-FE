480c0678d675a7e88ed5437e1b19dea96e83497e
Slime-Cat-24 <156169575+Slime-Cat-24@users.noreply.github.com>
2026-04-04 19:56:19 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig(function (_a) {
     var mode = _a.mode;
@@ -12,7 +12,7 @@ export default defineConfig(function (_a) {
         plugins: [react()],
         resolve: {
             alias: {
-                '@': path.resolve(__dirname, './src'),
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
         server: {
